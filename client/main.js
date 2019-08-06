c917f6881c924b5985861d25123f38726a3082ff
wilsonhj <wilsonhj@uci.edu>
2019-08-06 19:52:53 +0000

    merged food json

--- client/main.js
+++ client/main.js
@@ -1,18 +1,25 @@
 $(document).ready(startApp);
-
+let jsonFile = ``;
 function startApp(){
     getData();
 }
 
 function getData(){
+    json = 'getfood';
     $.ajax({
-        url: '../server/getnames.json',
+        url: `../server/${jsonFile}.json`,
         method: 'GET',
         dataType: 'json',
         success: handleDataFromServer
     })
 }
+
 function handleDataFromServer(response){
     console.log(response);
-    // makeNextCall();
+}
+function addDataToServer(request, response){
+    const dataPush = {
+        food: request.query.food,
+    }
+    response.send('done');
 }
